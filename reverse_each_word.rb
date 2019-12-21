d24dbba1243dbde89353b387df3792bf87e7dccc
Brigette LeVert <bnl.levert@gmail.com>
2019-12-21 21:39:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,9 @@ def reverse_each_word(sentence1)
   
   sentence1.split(' ')
   
- sentence1.each.reverse  do |sentence1|
+ sentence1.each  do |sentence1|
+   
+   puts sentence1.reverse 
   
 end
   
