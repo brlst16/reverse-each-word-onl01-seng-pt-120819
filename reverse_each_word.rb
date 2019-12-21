dbc8f80dd6ad85560e5a80989113256a2e953f50
Brigette LeVert <bnl.levert@gmail.com>
2019-12-21 21:32:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 sentence1 = ["Hello there, and how are you?"]
 
 
-def reverse_each_word(sentence1)
+def reverse_each_word(array)
   
-  puts reverse_each_word(sentence1)
+  puts reverse_each_word()
   
 end
   
