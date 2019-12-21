736872a01c52f803ef9df7fd741fd90020852774
Brigette LeVert <bnl.levert@gmail.com>
2019-12-21 21:45:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,9 @@ sentence1 = ["Hello","there", "and", "how", "are", "you?"]
 
 def reverse_each_word
   
+  sentence1 = ["Hello","there", "and", "how", "are", "you?"]
+
+  
   #sentence1.split(' ')
   
  #sentence1.each  do |sentence1|
